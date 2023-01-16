e07dab257d205abc3e68a17f132fe53227de79dd
Kuldeep-Abjima <119651692+Kuldeep-Abjima@users.noreply.github.com>
2023-01-16 18:59:10 +0530

    Add project files.

---
+++ AdoDotNet.16jan/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AdoDotNet._16jan")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AdoDotNet._16jan")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("34b43e7e-695c-4008-b9da-c1e76c172938")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ AdoDotNet.16jan/WebForm1.aspx.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Web.WebSockets;
+
+namespace AdoDotNet._16jan
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            string Cs = ConfigurationManager.ConnectionStrings["DBCS"].ConnectionString;
+            using (SqlConnection con = new SqlConnection(Cs))
+            {
+                con.Open();
+                SqlCommand cmd = new SqlCommand("select * from Products", con);
+                using(SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    DataTable table = new DataTable();
+                    table.Columns.Add("Id");
+                    table.Columns.Add("Name");
+                    table.Columns.Add("Price");
+                    table.Columns.Add("Discounted price");
+
+
+                    while (reader.Read())
+                    {
+
+                        DataRow dataRow = table.NewRow();
+
+                        int originalPrice = Convert.ToInt32(reader["price"]);
+                        double dicountedPrice = originalPrice * 90/100;
+
+
+                        dataRow["Id"] = reader["id"];
+                        dataRow["Name"] = reader["pname"];
+                        dataRow["Price"] = reader["price"];
+                        dataRow["Discounted Price"] = dicountedPrice;
+
+                        table.Rows.Add(dataRow);
+
+
+                    }
+
+                    GridView1.DataSource = table;
+                    GridView1.DataBind();
+                }
+            }
+            
+        }
+    }
+}
